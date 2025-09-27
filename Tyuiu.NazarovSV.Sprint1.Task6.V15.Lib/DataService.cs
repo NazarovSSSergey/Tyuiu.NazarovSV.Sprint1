ccfa5ca54b91aa7d5a877a9e2e35dd00063ccbc6
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-09-27 17:57:33 +0500

    Task6

---
+++ Tyuiu.NazarovSV.Sprint1.Task6.V15.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.NazarovSV.Sprint1.Task6.V15.Lib
+{
+    public class DataService : ISprint1Task6V15
+    {
+        public bool CheckLettersCount(string value)
+        {
+            int countL = 0;
+            int countS = 0;
+
+        }        
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint1.Task6.V15/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.NazarovSV.Sprint1.Task6.V15.Lib;
+namespace Tyuiu.NazarovSV.Sprint1.Task6.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                        *");
+            Console.WriteLine("**************************************************************************");
+
+            string value = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("* Результатом полученным после применения формулы является: " + ds.CheckLettersCount(value));
+            Console.ReadLine();
+        }
+    }
+}
